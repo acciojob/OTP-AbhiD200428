338f6a4c1f1a4842d8d5c805aff1b16897daa32d
Abhilash Dash <abhilash200428@gmail.com>
2026-03-30 16:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+
+  code.addEventListener('input', (e) => {
+    let value = e.target.value;
+
+    // Allow only single character
+    if (value.length > 1) {
+      value = value.slice(0, 1);
+      code.value = value;
+    }
+
+    if (value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener('keydown', (e) => {
+    if (e.key === "Backspace") {
+
+      if (code.value === "" && index > 0) {
+        codes[index - 1].focus();
+      } else {
+        code.value = "";
+      }
+
+    }
+  });
+
+});
